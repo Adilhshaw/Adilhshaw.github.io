a0e33f4713d09df009491398d21d6eb5cb311d5a
Adilhshaw <97376696+Adilhshaw@users.noreply.github.com>
2024-12-17 23:23:11 +0530

    Update service-worker.js

--- Whatsno/service-worker.js
+++ Whatsno/service-worker.js
@@ -1,22 +1,35 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing...');
-  event.waitUntil(
-    caches.open('whatsno-cache-v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/android-chrome-192x192.png',
-        '/android-chrome-512x512.png',
-      ]);
-    })
-  );
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('my-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/android-chrome-192x192.png',
+                '/android-chrome-512x512.png',
+            ]);
+        })
+    );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    var cacheWhitelist = ['my-cache'];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
