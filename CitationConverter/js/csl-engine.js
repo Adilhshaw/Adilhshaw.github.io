a9733915af6fb202405e856d819203a5ca013314
Adilhshaw <97376696+Adilhshaw@users.noreply.github.com>
2026-01-03 13:09:16 +0530

    Update csl-engine.js with error removl

--- CitationConverter/js/csl-engine.js
+++ CitationConverter/js/csl-engine.js
@@ -1,15 +1,13 @@
-async function formatCitation(cslData, style) {
+// CSL Engine and Metadata Normalisation
+
+window.formatCitation = async function (cslData, style) {
 
   const styleText = await fetch(`csl/${style}.csl`).then(r => r.text());
   const locale = await fetch("https://unpkg.com/citeproc/locales-en-US.xml").then(r => r.text());
 
   const sys = {
-    retrieveLocale: function () {
-      return locale;
-    },
-    retrieveItem: function (id) {
-      return cslData;
-    }
+    retrieveLocale: () => locale,
+    retrieveItem: () => cslData
   };
 
   const engine = new CSL.Engine(sys, styleText);
@@ -17,9 +15,9 @@ async function formatCitation(cslData, style) {
 
   const bibliography = engine.makeBibliography();
   return bibliography[1][0];
-}
+};
 
-function makeCSL(item) {
+window.makeCSL = function (item) {
   return {
     id: item.DOI || "item1",
     type: item.type || "article-journal",
@@ -29,11 +27,10 @@ function makeCSL(item) {
       given: a.given
     })),
     issued: { "date-parts": [[ item.issued["date-parts"][0][0] ]] },
-    container-title: item["container-title"]?.[0],
+    "container-title": item["container-title"]?.[0],
     volume: item.volume,
     issue: item.issue,
     page: item.page,
     DOI: item.DOI
   };
-}
-console.log("csl-engine loaded");
+};
