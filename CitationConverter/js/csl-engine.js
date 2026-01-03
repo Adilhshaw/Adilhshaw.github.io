6f575a1532043a99d16912e7f2c15dd1c66cb9d6
Adilhshaw <97376696+Adilhshaw@users.noreply.github.com>
2026-01-03 13:00:08 +0530

    Create csl-engine.js

---
+++ CitationConverter/js/csl-engine.js
@@ -0,0 +1,39 @@
+async function formatCitation(cslData, style) {
+
+  const styleText = await fetch(`csl/${style}.csl`).then(r => r.text());
+  const locale = await fetch("https://unpkg.com/citeproc/locales-en-US.xml").then(r => r.text());
+
+  const sys = {
+    retrieveLocale: function () {
+      return locale;
+    },
+    retrieveItem: function (id) {
+      return cslData;
+    }
+  };
+
+  const engine = new CSL.Engine(sys, styleText);
+  engine.updateItems([cslData.id]);
+
+  const bibliography = engine.makeBibliography();
+  return bibliography[1][0];
+}
+
+function makeCSL(item) {
+  return {
+    id: item.DOI || "item1",
+    type: item.type || "article-journal",
+    title: item.title?.[0],
+    author: item.author?.map(a => ({
+      family: a.family,
+      given: a.given
+    })),
+    issued: { "date-parts": [[ item.issued["date-parts"][0][0] ]] },
+    container-title: item["container-title"]?.[0],
+    volume: item.volume,
+    issue: item.issue,
+    page: item.page,
+    DOI: item.DOI
+  };
+}
+console.log("csl-engine loaded");
